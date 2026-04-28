91bb9cfd7787144c3d82782414f58e585b121d26
Angel Serrato <serrato.anggel@gmail.com>
2026-04-28 12:12:24 -0500

    chore: initialize project with vite, react, and tailwindcss

---
+++ src/components/SinglePriceGrid.jsx
@@ -0,0 +1,23 @@
+function SinglePriceGrid() {
+  return (
+    <main>
+      <p>Join our community</p>
+      <p>30-day, hassle-free money back guarantee</p>
+      <p>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+      <p>Monthly Subscription</p>
+      <p>&dollar;29 per month</p>
+      <p>Full access for less than &dollar;1 a day</p>
+      <p>Sign Up</p>
+      <p>Why Us</p>
+      <p>Tutorials by industry experts</p>
+      <p>Peer &amp; expert code review</p>
+      <p>Coding exercises</p>
+      <p>Access to our GitHub repos</p>
+      <p>Community forum</p>
+      <p>Flashcard decks</p>
+      <p>New videos every week</p>
+    </main>
+  );
+}
+
+export default SinglePriceGrid;
